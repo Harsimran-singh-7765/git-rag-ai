4dc0ca7b0b758fda41c97ca74023f966d8050c53
Harsimran-singh-7765 <singhharry7765@gmail.com>
2025-06-19 00:18:29 +0530

    FIX-trial

--- utilis.py
+++ utilis.py
@@ -4,17 +4,68 @@ from collections import Counter
 
 # Basic extension to language mapping
 EXT_TO_LANG = {
-    ".py": "Python",
-    ".js": "JavaScript",
-    ".ts": "TypeScript",
-    ".java": "Java",
-    ".cpp": "C++",
-    ".c": "C",
+    # Web & Frontend
     ".html": "HTML",
+    ".htm": "HTML",
     ".css": "CSS",
-    ".json": "JSON",
+    ".scss": "SASS",
+    ".sass": "SASS",
+    ".less": "LESS",
+    ".js": "JavaScript",
+    ".mjs": "JavaScript",
+    ".ts": "TypeScript",
+    ".tsx": "TypeScript JSX",
+    ".jsx": "JavaScript JSX",
 
+    # Backend & Scripting
+    ".py": "Python",
+    ".rb": "Ruby",
+    ".php": "PHP",
+    ".java": "Java",
+    ".kt": "Kotlin",
+    ".go": "Go",
+    ".rs": "Rust",
+    ".c": "C",
+    ".cpp": "C++",
+    ".cs": "C#",
+    ".sh": "Shell",
+    ".bat": "Batch",
+    ".pl": "Perl",
+    ".lua": "Lua",
+
+    # Data & Config
+    ".json": "JSON",
+    ".yaml": "YAML",
+    ".yml": "YAML",
+    ".xml": "XML",
+    ".toml": "TOML",
+    ".ini": "INI",
+    ".env": "ENV",
+
+    # Markup & Docs
+    ".md": "Markdown",
+    ".rst": "reStructuredText",
+    ".txt": "Text",
+
+    # Notebook & Data Science
+    ".ipynb": "Jupyter Notebook",
+    ".r": "R",
+    ".jl": "Julia",
+
+    # SQL & DB
+    ".sql": "SQL",
+
+    # Other
+    ".dockerfile": "Dockerfile",
+    "Dockerfile": "Dockerfile",  # Special case
+    ".makefile": "Makefile",
+    "Makefile": "Makefile",      # Special case
+
+    # Log & Ignore
+    ".log": "Log File",
+    ".gitignore": "Git Ignore",
 }
+
 
 def analyze_languages(repo_path: str) -> dict:
     """Analyzes file extensions to estimate language usage."""
